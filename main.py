421d73fdbf584d82fc997f85adf4055754633f27
StephenZahra <stephenzahra101@gmail.com>
2023-02-08 23:29:23 +0100

    Successfully detecting stored posterior SSTI

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import urllib.request
 import requests
 
 
-def get_request():
+def begin_scan():
     """
-    Sends a get request to the page returning the url used and html page
+    Begins the scanning process by getting the html for a given site
     """
-
     url = input("Please enter a url: ")
     response = urllib.request.urlopen(url)
     html = response.read()
 
-    return url, html
+    return html
+
+
+def get_html(url):
+    """
+    This function acquires the html for any given url
+    """
+    response = urllib.request.urlopen(url)
+    html = response.read()
+
+    return html
 
 
 def get_page_urls(html):
@@ -30,6 +39,23 @@ def get_page_urls(html):
             required_links.append(modif_link)
 
     return required_links
+
+
+def check_nested_links(required_links):
+    nested_links = []
+    for url in required_links:
+        response = urllib.request.urlopen(url)
+        html = response.read()
+
+        link_collection = re.findall(r"(href[a-zA-Z0-9 _=:.\"/'\\\\]*)+", str(html))
+
+        for link in link_collection:
+            if (link.find("127.0.0.1:8000") != -1):
+                modif_link = link.replace("href=\"", '')
+                modif_link = modif_link.replace("\"", '')
+                nested_links.append(modif_link)
+    return nested_links
+
 
 def locate_input_points(html):
     """
@@ -50,7 +76,7 @@ def locate_input_points(html):
         all_names = re.findall(r"(name=[\\\\'a-zA-Z\\\\']+)", inp)
 
     final_names = []
-    for elem in all_names:
+    for elem in all_names:  # Clean names
         temp_name = elem.replace("name=\\'", "")
         temp_name = temp_name.replace("\\'", "")
         final_names.append(temp_name)
@@ -83,32 +109,62 @@ def test_reflected(url, names):
 
     return scan_result
 
-def test_stored_posterior(target_url, names, urls):
+
+def filter_stored_posterior_urls(all_urls):
+    """
+    This function checks for duplicates in the total collection of links in the website and removes them
+    """
+    for x in all_urls:
+        if(any(all_urls.count(x) > 1 for x in all_urls) == True):
+            all_urls.remove(x)
+    return all_urls
+
+
+def test_stored_posterior(urls):
     """
     This function performs SSTI on a given target url for all forms, and checks each page to verify if any of it has
     executed
     """
-    session = requests.session()
-    front = session.get(target_url)
+    for url in urls:
+        session = requests.session()
+        front = session.get(url)
 
-    token = re.findall(r'<input type="hidden" name="_token" value="(.*)"', front.text)[0]
-    cookies = session.cookies
+        token = re.findall(r'<input type="hidden" name="_token" value="(.*)"', front.text)[0]
+        cookies = session.cookies
 
-    scan_result = {}  # dictionary to store results
-    for name in names:
-        data = {name: "{{7*7}}", "_token": token}
-        req = requests.post(target_url, data=data, cookies=cookies)
-        text = req.text
-        print(text)
+        inputs = locate_input_points(front)
+
+        for name in inputs:
+            data = {name: "{{7*7}}", "_token": token}
+            requests.post(url, data=data, cookies=cookies)
+
+        scan_result = {}
+        for next_url in urls:
+            html = get_html(next_url)
+
+            if("49" in str(html)):
+                scan_result[next_url] = True
+            else:
+                scan_result[next_url] = False
+
+        return scan_result
 
 
-url, html = get_request()
+
+# Begin the scanning process
+html = begin_scan()
 
 names = locate_input_points(html)
 urls = get_page_urls(html)
-print(urls)
+
 #results = test_reflected(url, names)
-#test_stored_posterior("http://127.0.0.1:8000/", names, urls)
 
 #for result in results:  # print results
  #   print("Input name: " + result + " isVulnerable: " + str(results[result]))
+
+nested_links = check_nested_links(urls)
+filtered_urls = filter_stored_posterior_urls(urls+nested_links)
+stored_pos_results = test_stored_posterior(filtered_urls)
+
+for result in stored_pos_results:  # print results
+   print("URL: " + result + " hasExecutedSSTI: " + str(stored_pos_results[result]))
