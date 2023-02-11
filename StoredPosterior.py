ced7caad45a855d9b87fecf66a7443f2794fa252
StephenZahra <stephenzahra101@gmail.com>
2023-02-11 22:06:22 +0100

    Fixed bugs

--- StoredPosterior.py
+++ StoredPosterior.py
@@ -1,7 +1,6 @@
 import re
 import urllib.request
 import requests
-import subprocess
 
 
 def begin_scan():
@@ -10,6 +9,17 @@ def begin_scan():
     """
 
     url = input("Please enter a url: ")
+    response = urllib.request.urlopen(url)
+    html = response.read()
+
+    return html
+
+
+def get_html(url):
+    """
+    This function acquires the html for any given url
+    """
+
     response = urllib.request.urlopen(url)
     html = response.read()
 
@@ -70,14 +80,13 @@ def locate_input_points(html):
             input_collection.remove(elem)
 
     for inp in input_collection:  # filter through remaining valid inputs storing the names
-        all_names = re.findall(r"(name=[\\\\'a-zA-Z0-9\"'\\\\']+)", str(inp))
+        all_names = re.findall(r"(name=[\\\\'a-zA-Z0-9\"'\\\\']+)", inp)
 
     final_names = []
     for elem in all_names:  # Clean names
         temp_name = elem.replace("name=", "")
         temp_name = temp_name.replace("'", "")
         final_names.append(temp_name)
-
     return final_names
 
 
@@ -90,10 +99,10 @@ def filter_links(all_urls):
     return all_urls
 
 
-def test_reflected(urls):
+def test_stored_posterior(urls):
     """
-    Repeatedly sends POST requests to the page using previously acquired input tag names, returns dictionary
-    with boolean values
+    This function performs SSTI on a given target url for all forms, and checks each page to verify if any of it has
+    executed
     """
 
     for url in urls:
@@ -105,18 +114,21 @@ def test_reflected(urls):
 
         inputs = locate_input_points(front.text)
 
-        scan_result = {}  # dictionary to store results
         for name in inputs:
             data = {name: "{{7*7}}", "_token": token}
-            req = requests.post(url, data=data, cookies=cookies)
-            text = req.text
+            requests.post(url, data=data, cookies=cookies)
 
-            if "49" in text:
-                scan_result[name] = True
+        scan_result = {}
+        for next_url in urls:
+            html = get_html(next_url)
+
+            if("49" in str(html)):
+                scan_result[next_url] = True
             else:
-                scan_result[name] = False
+                scan_result[next_url] = False
 
         return scan_result
+
 
 
 # Begin the scanning process
@@ -126,11 +138,7 @@ urls = get_page_urls(html)
 
 nested_links = check_nested_links(urls)
 filtered_urls = filter_links(urls+nested_links)
-results = test_reflected(filtered_urls)
+stored_pos_results = test_stored_posterior(filtered_urls)
 
-print("Testing Reflected SSTI")
-for result in results:  # print results
-    print("Input name: " + result + " isVulnerable: " + str(results[result]))
-
-print("\n\nTesting Stored SSTI with Posterior Injection and Rendering")
-subprocess.run('python StoredPosterior.py')
+for result in stored_pos_results:  # print results
+   print("URL: " + result + " hasExecutedSSTI: " + str(stored_pos_results[result]))
